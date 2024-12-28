6642f8b12584466faa5b9728dc1f4793e8d2e254
TM <temenovbekzan@gmail.com>
2024-12-28 15:30:28 +0600

    исправил router

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,7 +1,15 @@
 <template>
-    <div class="h-screen bg-amber-200 min-w-[300px] py-[50px] px-[20px] flex flex-col gap-3">
-        <a href="/products" class="text-xl cursor-pointer">Продукты</a>
-        <a href="/" class="text-xl cursor-pointer">Заказы</a>
-        <a href="/wherehouse" class="text-xl cursor-pointer">Склады</a>
-    </div>
+  <div
+    class="h-screen bg-amber-200 min-w-[300px] py-[50px] px-[20px] flex flex-col gap-3"
+  >
+    <router-link to="/products" class="text-xl cursor-pointer">
+      Продукты
+    </router-link>
+    <router-link to="/" class="text-xl cursor-pointer">
+    Заказы
+    </router-link>
+    <router-link to="/wherehouse" class="text-xl cursor-pointer">
+   Склады
+    </router-link>
+  </div>
 </template>
